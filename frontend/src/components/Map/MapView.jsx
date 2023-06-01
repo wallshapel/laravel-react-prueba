b95cfb0534516eda94661249b4608683f3d2a228
Legato <abelyimartinez1987@hotmail.com>
2023-06-01 14:23:37 -0500

    Mejor marcador

--- frontend/src/components/Map/MapView.jsx
+++ frontend/src/components/Map/MapView.jsx
@@ -6,7 +6,6 @@ import 'leaflet/dist/leaflet.css';
 const MapView = ({ coord, humidities }) => {
 
 	if (Object.entries(coord).length !== 0) {
-
 		// 40.500141, -97.422813 // Centro de los estados unidos
 		return	<MapContainer center={ { lat: 40.500141, lng: -97.422813 } } zoom= { 4 }>
 			<TileLayer 
--- frontend/src/components/Map/Markers.jsx
+++ frontend/src/components/Map/Markers.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { Marker, Tooltip } from 'react-leaflet';
-import L from 'leaflet';
-
-const markerIcon = new L.Icon({
-  iconUrl: require('./marker.png'),
-  iconSize: [20, 20],
-  iconAnchor: [17, 46], //[left/right, top/bottom]
-  popupAnchor: [0, -46], //[left/right, top/bottom]
-});
+import { Marker, Tooltip, Popup } from 'react-leaflet';
+import markerIconPng from 'leaflet/dist/images/marker-icon.png';
+import { Icon } from 'leaflet';
 
 const Markers = ({ coord, humidities }) => {
 
 	if (humidities.length === coord.length) {
 		const markers = coord.map((value, i) => (
-			<Marker key={ i } position={ { lat: value.lat, lng: value.lon } } icon={ markerIcon } >
-				<Tooltip>
+			<Marker key={ i } position={ { lat: value.lat, lng: value.lon } } icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
+				<Popup>Humidity: { humidities[i].humidity }%</Popup>
+				<Tooltip offset={[0, -40]} >
 			  	<span>Humidity: { humidities[i].humidity }%</span>
 			  </Tooltip>	
 			</Marker>	
